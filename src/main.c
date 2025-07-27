aa66505ee6aa0abd5327a66cc4ca854ffd436484
sebs-tech <sebastjan.rijavec@gmail.com>
2025-07-27 23:42:32 +0200

    clang format

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #define MAX_STRING_LENGTH 80
 
 int main(int argc, char *argv[]) {
-	
+
   char temp_buffer[MAX_STRING_LENGTH + 1];
 
   // Seed the random number generator only once
@@ -22,49 +22,47 @@ int main(int argc, char *argv[]) {
   strncpy(temp_buffer, curses[rnum], MAX_STRING_LENGTH);
   temp_buffer[MAX_STRING_LENGTH] = '\0'; // Ensure null-termination
 
-	/*
-		flags:
-			-c: captialise 
-			-a: captitalise_all_letters
-		  -r: capitalise_every_second_char
-			-x: censor 
-	*/	
+  /*
+          flags:
+                  -c: captialise
+                  -a: captitalise_all_letters
+            -r: capitalise_every_second_char
+                  -x: censor
+  */
 
-	// no flags
-	if (argc == 1) {
-		printf("%s ", temp_buffer);
-		return 0;
-	}
+  // no flags
+  if (argc == 1) {
+    printf("%s ", temp_buffer);
+    return 0;
+  }
 
-	// -c flag
-	if (strcmp(argv[1], "-c") == 0) {
-		capitalise(temp_buffer);
-		printf("%s ", temp_buffer);
-		return 0;
-	}
+  // -c flag
+  if (strcmp(argv[1], "-c") == 0) {
+    capitalise(temp_buffer);
+    printf("%s ", temp_buffer);
+    return 0;
+  }
 
-	// -a flag 
-	if (strcmp(argv[1], "-a") == 0) {
-		capitalise_all_letters(temp_buffer);
-		printf("%s ", temp_buffer);
-		return 0;
-	}
+  // -a flag
+  if (strcmp(argv[1], "-a") == 0) {
+    capitalise_all_letters(temp_buffer);
+    printf("%s ", temp_buffer);
+    return 0;
+  }
 
-	// -r flag 
-	if (strcmp(argv[1], "-r") == 0) {
-		capitalise_every_second_char(temp_buffer);
-		printf("%s ", temp_buffer);
-		return 0;
-	}
+  // -r flag
+  if (strcmp(argv[1], "-r") == 0) {
+    capitalise_every_second_char(temp_buffer);
+    printf("%s ", temp_buffer);
+    return 0;
+  }
 
-	// -x flag 
-	if (strcmp(argv[1], "-x") == 0) {
-		censor(temp_buffer);
-		printf("%s ", temp_buffer);
-		return 0;
-	}
-
-
+  // -x flag
+  if (strcmp(argv[1], "-x") == 0) {
+    censor(temp_buffer);
+    printf("%s ", temp_buffer);
+    return 0;
+  }
 
   /* exit */
 
